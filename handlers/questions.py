40e6a9f0dcd90a2cda935190c386976864c165e8
Dabbler10 <dabbler503@gmail.com>
2024-09-11 16:18:18 +0500

    demo

--- handlers/questions.py
+++ handlers/questions.py
@@ -1,8 +1,9 @@
 from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardRemove
-from keyboards.for_questions import get_yes_no_kb
+from keyboards.for_questions import make_row_kb
 
+subject_list = ['Матан', 'Тервер', 'Алгем']
 router = Router()
 
 @router.message(Command("start"))
@@ -16,3 +17,32 @@ async def cmd_help(message: Message):
     await message.answer(
         "Напишите предмет и я попытаюсь найти конспекты по нему :)"
     )
+
+@router.message(Command("add"))
+async def cmd_add(message: Message):
+    await message.answer(
+        "Выбери предмет из списка или добавь свой",
+        reply_markup=make_row_kb(subject_list)
+    )
+
+@router.message(F.text.lower() == "алгем")
+async def answer_yes(message: Message):
+    await message.answer(
+        "Это здорово!",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+@router.message(F.text.lower() == "матан")
+async def answer_no(message: Message):
+    await message.answer(
+        "Жаль...",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+@router.message(F.text.lower() == "тервер")
+async def answer_no(message: Message):
+    await message.answer(
+        "Жаль...",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
--- keyboards/for_questions.py
+++ keyboards/for_questions.py
@@ -1,9 +1,5 @@
-from aiogram.types import ReplyKeyboardMarkup
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-def get_yes_no_kb() -> ReplyKeyboardMarkup:
-    kb = ReplyKeyboardBuilder()
-    kb.button(text="Да")
-    kb.button(text="Нет")
-    kb.adjust(2)
-    return kb.as_markup(resize_keyboard=True)
+def make_row_kb(items: list[str]) -> ReplyKeyboardMarkup:
+    row = [KeyboardButton(text=item) for item in items]
+    return ReplyKeyboardMarkup(keyboard=[row], resize_keyboard=True)
