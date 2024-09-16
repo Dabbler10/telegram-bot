58a02bd1d50f8af5d54212b85018e11ce90f7aa7
Dabbler10 <dabbler503@gmail.com>
2024-09-16 16:45:01 +0500

    webhook

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-import asyncio
+import json
+import os
+from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
-from config_reader import config
 import logging
 from handlers import questions
 
+load_dotenv()
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)')
-bot = Bot(token=config.bot_token.get_secret_value())
+
+bot = Bot(os.getenv('BOT_TOKEN'))
 dp = Dispatcher()
 dp.include_routers(questions.router)
 
-async def main():
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
 
-if __name__ == '__main__':
-    asyncio.run(main())
+async def handler(event, context):
+    update = json.loads(event['body'])
+    my_update = types.Update(update_id=update['update_id'], message=update['message'])
+    await dp.feed_update(bot=bot, update=my_update)
+
+    return {
+        'statusCode': 200,
+        'body': 'ok!'
+    }
